13d432c3f4675ca215244d02ca11828644f45b47
Tri <vutriminh@gmail.com>
2025-04-07 16:33:58 -0400

    added a not-found page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout() {
       <StatusBar style="dark" />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
+        <Stack.Screen name="+not-found" />
         <Stack.Screen name="sameNumber/[id]" />
         <Stack.Screen name="multipleChoice/[id]" />
       </Stack>
